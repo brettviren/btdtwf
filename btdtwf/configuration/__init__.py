d6fc37d7a16a43c3caee18ed49c7552058bf1e11
Brett Viren <bv@bnl.gov>
2013-08-08 17:36:02 -0400

    Clean up interpretation of configuration file.

--- btdtwf/configuration/__init__.py
+++ btdtwf/configuration/__init__.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
-import btdtwf
+from collections import namedtuple
+from btdtwf import Graph
+from btdtwf.util import format_flat_dict
 import ConfigParser
 NoSectionError = ConfigParser.NoSectionError
+NoOptionError = ConfigParser.NoOptionError
+
+WorkflowConfig = namedtuple('WorkflowConfig','name nodes input_nodes params workdir')
+NodeConfig = namedtuple('NodeConfig', 'name object input_nodes ctor kwdargs')
 
 def comma_list_split(string):
     if not string:
@@ -10,32 +16,52 @@ def comma_list_split(string):
     ret = [x.strip() for x in string.split(',')]
     return [x for x in ret if x]
 
-def add_node_dict(nodes, cfg, name):
-    if name in nodes.keys(): 
-        return
-
-    node_section_name = 'node %s'%name
-    nodes[name] = dict(cfg.items(node_section_name), name=name)
-
-    if not cfg.has_option(node_section_name, 'input_nodes'):
-        return
-
-    node_list = cfg.get(node_section_name, 'input_nodes')
-    for node_name in comma_list_split(node_list):
-        add_node_dict(nodes, cfg, node_name)
-
-def parse(filename, **params):
+def parse(filename):
+    'Return a configuration object made by parsing filename (or list of filenames)'
     cfg = ConfigParser.SafeConfigParser()
     cfg.optionxform = str       # why would we want to lose case?
     cfg.read(filename)
+    return cfg
 
-    workflow = None
+
+def make_node_object(ctor, **kwds):
+    mn, nc = ctor.rsplit('.',1)
+    importstr = 'from %s import %s' % (mn,nc)
+    exec (importstr)
+    node_ctor = eval(nc)
+    obj = node_ctor(**kwds)
+    return obj
+
+
+def add_node_dict(nodes, cfg, name, **kwds):
+    if name in nodes.keys(): 
+        return                  # already got it
+
+    node_section_name = 'node %s'%name
+    nsecdict = dict(cfg.items(node_section_name))
+
+    try:
+        input_nodes = nsecdict.pop('input_nodes')
+    except KeyError:
+        input_nodes = []
+    else:
+        input_nodes = comma_list_split(input_nodes)
+        for dep_name in input_nodes:
+            add_node_dict(nodes, cfg, dep_name, **kwds)
+
+    ctor = nsecdict.pop('constructor') # required node section item
+    ctor_kwds = format_flat_dict(nsecdict, **kwds)
+    obj = make_node_object(ctor, **ctor_kwds)
+    nodes[name] = NodeConfig(name=name, object=obj, input_nodes=input_nodes, ctor=ctor, kwdargs=ctor_kwds)
+
+
+def get_workflow_name(cfg, **params):
+    'Return the requested workflow name, raise NoSectionError if not found in the configuration object'
     workflows = []
-    wfsectype = 'workflow '
     for sec in cfg.sections():
-        if sec.startswith(wfsectype):
-            workflows.append(sec[len(wfsectype):])
-
+        if sec.startswith('workflow '):
+            workflows.append(sec[len('workflow '):])
+    workflow = None
     if workflows:
         workflow = workflows[0]
     if cfg.has_section('defaults'):
@@ -43,48 +69,78 @@ def parse(filename, **params):
     if params.has_key('workflow'):
         workflow = params.get('workflow')
     if not workflow in workflows:
-        raise NoSectionError, 'No such workflow "%s" defined in %s' % (workflow, filename)
+        raise NoSectionError, 'No such workflow "%s" defined' % (workflow, )
+    return workflow
 
-    params.update(dict(cfg.items('workflow %s' % workflow)))
-    params['workflow'] = workflow
+def get_params(cfg, params_name):
+    'Return a dictionary of parameters section values'
+    try:
+        parameters = cfg.items('parameters %s' % params_name)
+    except NoSectionError:
+        return dict()
+    p = dict(parameters)
+    try:
+        more = p.pop('parameters')
+    except KeyError:
+        return p
+    # coalesce daughters before updating mother
+    more_p = dict()
+    for sp in comma_list_split(more):
+        more_p.update(get_params(cfg, sp))
+    p.update(more_p)
+    return p
 
+def get_workflow_params(cfg, workflow):
+    'Return a flattened and formatted dictionary of the workflow parameters'
+    ret = dict()
+    try:
+        plist = cfg.get('workflow '+workflow, 'parameters')
+    except NoOptionError:
+        return ret
+
+    for psec in comma_list_split(plist):
+        ret.update(get_params(cfg, psec))
+    return ret
+
+def get_workflow_nodes(cfg, workflow, **kwds):
+    input_nodes = cfg.get('workflow '+workflow, 'input_nodes') # required node section item
+    ret = dict()
+    for nname in comma_list_split(input_nodes):
+        add_node_dict(ret, cfg, nname, **kwds)
+    return ret
+
+def interpret(cfg, **extra_params):
+    workflow_name = get_workflow_name(cfg, **extra_params)
+    params = get_workflow_params(cfg, workflow_name)
+    params['workflow'] = workflow_name
+    params_ff = format_flat_dict(params, **extra_params)
+
+    #nodes = get_workflow_nodes(cfg, **params_ff)
+    input_node_names = cfg.get('workflow '+workflow_name, 'input_nodes') # required node section item
+    input_node_names = comma_list_split(input_node_names)
     nodes = dict()
-    node_list = comma_list_split(params.get('input_nodes'))
-    for node_name in node_list:
-        add_node_dict(nodes, cfg, node_name)
-    params['nodes'] = nodes
-    params['input_nodes'] = node_list
 
-    return params
+    for nname in input_node_names:
+        add_node_dict(nodes, cfg, nname, **params_ff)
 
-def node_dict2obj(nd):
-    callable_name = nd['callable']
-    mn, nc = callable_name.rsplit('.',1)
-    importstr = 'from %s import %s' % (mn,nc)
-    print importstr
-    exec (importstr)
-    node_ctor = eval(nc)
-    obj = node_ctor(**nd)
-    nd['object'] = obj
-    return obj
+    params_nt = namedtuple('params',params.keys())(**params_ff)
+    return WorkflowConfig(name=workflow_name, nodes = nodes, input_nodes=input_node_names,
+                          params = params_nt, 
+                          workdir = params_ff.get('workdir','.'))
 
-def make_graph(**kwds):
-    node_dicts = kwds.get('nodes', dict())
-    graph = btdtwf.nx.Graph()
+
+def make_graph(wf):
+    graph = Graph()
 
     # initial node addition
-    node_objs = dict()
-    for node_name, node_dict in node_dicts.items():
-        node = node_dict2obj(node_dict)
-        node_objs[node_name] = node
-        graph.add_node(node)
+    for node in wf.nodes.values():
+        graph.add_node(node.object)
         
     # hook up edges
-    for node_name, node_dict in node_dicts.items():
-        this_node = node_objs[node_name]
-        for onn in comma_list_split(node_dict.get('input_nodes')):
-            other_node = node_objs[onn]
-            graph.add_edge(other_node, this_node)
+    for node in wf.nodes.values():
+        for node_name in node.input_nodes:
+            other_node = wf.nodes[node_name]
+            graph.add_edge(other_node.object, node.object)
 
     return graph
 
--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -7,22 +7,27 @@ testdir = os.path.dirname(__file__)
 test_cfg_file = os.path.join(testdir, 'test_wf.cfg')
 
 def test_parse():
-    wf = configuration.parse(test_cfg_file)
-    assert 3 == len(wf['nodes']), str(wf['nodes'].keys())
-    wf = configuration.parse(test_cfg_file, workflow='wf2')
-    assert set(['nodeD']) == set(wf['input_nodes']), str(wf['input_nodes'])
+    cfg = configuration.parse(test_cfg_file)
+    wf = configuration.interpret(cfg)
+    assert 3 == len(wf.input_nodes), str(wf.input_nodes)
+    cfg = configuration.parse(test_cfg_file)
+    wf = configuration.interpret(cfg, workflow='wf2')
+    assert ['nodeD'] == wf.input_nodes, wf.input_nodes
 
 def test_fail():
+    cfg = configuration.parse(test_cfg_file)
     try:
-        configuration.parse(test_cfg_file, workflow = "doesnotexist")
+        configuration.interpret(cfg , workflow = "doesnotexist")
     except configuration.NoSectionError:
         print 'Raised NoSectionError as expected'
     else:
         raise 'ValueError: should have raised NoSectionError'
 
 def test_graph():
-    wf = configuration.parse(test_cfg_file, workflow='wf2')
-    g = configuration.make_graph(**wf)
+    cfg = configuration.parse(test_cfg_file)
+    wf = configuration.interpret(cfg, workflow='wf2')
+    g = configuration.make_graph(wf)
+
     for n1,n2,d in g.edges(data=True):
         print n1,n2,d
     connect(g)
@@ -30,12 +35,12 @@ def test_graph():
     for n1,n2,d in g.edges(data=True):
         print n1,n2,d
 
-    for name,node in wf['nodes'].items():
-        print name,node
+    for name,nc in wf.nodes.items():
+        print name, nc
 
-    for node_name in wf['input_nodes']:
-        node = wf['nodes'][node_name]
-        node['object']()
+    for node_name in wf.input_nodes:
+        node = wf.nodes[node_name]
+        node.object()
 
 
 if '__main__' == __name__:
